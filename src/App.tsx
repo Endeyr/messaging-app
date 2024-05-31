82524155e83c7cb7f0d20b743ee7f13bf3e93e61
Endeyr <aaron.thompson42594@gmail.com>
2024-05-31 10:22:44 -0400

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import Button from '@mui/material/Button';
+
 function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+	return <Button variant="contained">Hello world</Button>;
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+import '@fontsource/roboto'
+import CssBaseline from '@mui/material/CssBaseline'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<CssBaseline />
+		<App />
+	</React.StrictMode>
 )
