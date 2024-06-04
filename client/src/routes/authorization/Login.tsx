bd4f3f94c8de7f57a718046400f21d544a47dba8
Endeyr <aaron.thompson42594@gmail.com>
2024-06-04 12:41:54 -0400

    basic logic for login

--- client/src/routes/authorization/Login.tsx
+++ client/src/routes/authorization/Login.tsx
@@ -1,4 +1,25 @@
+import { useState } from 'react'
+
 const Login = () => {
-	return <div>Login</div>
+	const [username, setUsername] = useState('')
+	return (
+		<div>
+			<h1>Login</h1>
+			<form
+				onSubmit={(e) => {
+					e.preventDefault()
+					// logic to set username to Context or local storage or db
+				}}
+			>
+				<input
+					type="text"
+					value={username}
+					placeholder="username"
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	)
 }
 export default Login
