4ca3b65990c4b8428ad02b36ef867b1c370608b2
Endeyr <aaron.thompson42594@gmail.com>
2024-06-12 16:28:58 -0400

    add basic get and post messages functionality

--- server/routes/messageController.ts
+++ server/routes/messageController.ts
@@ -1,24 +1,43 @@
 import { NextFunction, Request, Response } from 'express'
 import Message from '../model/messages'
+import { UserAuthRequest } from './../types'
 
 export const getMessages = async (
-	req: Request,
+	req: UserAuthRequest,
 	res: Response,
 	next: NextFunction
 ) => {
-	res.json({ message: 'Get all messages' })
+	let messages
+	if (!req.user) {
+		res.json({ message: 'User not found' })
+	} else {
+		messages = await Message.find({ user: req.user.id })
+		res.json({ messages })
+	}
 }
 
 export const sendMessage = async (
-	req: Request,
+	req: UserAuthRequest,
 	res: Response,
 	next: NextFunction
 ) => {
-	res.json({ message: 'Send a message' })
+	if (!req.body.text) {
+		res.status(400)
+		res.json({ message: 'Please add a text field' })
+	}
+	if (!req.user) {
+		res.json({ message: 'User not found' })
+	} else {
+		const message = await Message.create({
+			text: req.body.text,
+			user: req.user.id,
+		})
+		res.status(200).json(message)
+	}
 }
 
 export const deleteMessage = async (
-	req: Request,
+	req: UserAuthRequest,
 	res: Response,
 	next: NextFunction
 ) => {
--- server/routes/messageRoutes.ts
+++ server/routes/messageRoutes.ts
@@ -5,7 +5,7 @@ import { deleteMessage, getMessages, sendMessage } from './messageController'
 const messageRouter = express.Router()
 
 messageRouter.get('/', protect, getMessages)
-messageRouter.post('/:id', protect, sendMessage)
+messageRouter.post('/', protect, sendMessage)
 messageRouter.delete('/:id', protect, deleteMessage)
 
 export default messageRouter
