71b23fbfada66223ae02b5a3bdd50d73e89a85a9
Endeyr <aaron.thompson42594@gmail.com>
2024-06-12 16:15:27 -0400

    protect route

--- server/routes/messageRoutes.ts
+++ server/routes/messageRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express'
+import { protect } from '../middleware/authMiddleware'
 import { deleteMessage, getMessages, sendMessage } from './messageController'
 
 const messageRouter = express.Router()
 
-messageRouter.get('/', getMessages)
-messageRouter.post('/', sendMessage)
-messageRouter.delete('/', deleteMessage)
+messageRouter.get('/', protect, getMessages)
+messageRouter.post('/:id', protect, sendMessage)
+messageRouter.delete('/:id', protect, deleteMessage)
 
 export default messageRouter
