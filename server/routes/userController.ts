e72749fc25a4bf54107fe82d6916dadbb1465d1b
Endeyr <aaron.thompson42594@gmail.com>
2024-06-13 15:08:36 -0400

    add update and delete user

--- server/routes/userController.ts
+++ server/routes/userController.ts
@@ -2,7 +2,7 @@ import bcrypt from 'bcryptjs'
 import { NextFunction, Request, Response } from 'express'
 import jwt from 'jsonwebtoken'
 import User from '../model/user'
-import { UserAuthRequest } from './../types'
+import { RoleEnum, UserAuthRequest } from './../types'
 
 // @desc Register new user
 // @route POST /api/user/register
@@ -62,7 +62,6 @@ export const loginUser = async (
 	res: Response,
 	next: NextFunction
 ) => {
-	// Handle user login logic using validated data from req.body
 	const { email, password } = req.body
 	let existingUser
 	try {
@@ -107,15 +106,68 @@ export const loginUser = async (
 // @desc Delete user account
 // @route POST /api/user/delete
 // @access Private
-export const deleteUser = (req: Request, res: Response, next: NextFunction) => {
-	// logic for deleting a users account
+export const deleteUser = async (
+	req: UserAuthRequest,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		if (!req.params.id) {
+			return res.status(400).json({ message: 'No id provided' })
+		}
+		const user = await User.findById(req.params.id)
+		if (!user) {
+			return res.status(400).json({ message: 'User not found' })
+		}
+		if (!req.user?.role.includes(RoleEnum.admin)) {
+			return res
+				.status(401)
+				.json({ message: 'User not authorized to delete user' })
+		}
+		await user.deleteOne()
+		return res
+			.status(200)
+			.json({ id: req.params.id, message: 'User deleted successfully ' })
+	} catch (error) {
+		console.error('Error deleting user:', error)
+		return res.status(500).json({ message: 'Server error' })
+	}
 }
 
 // @desc Update user account
 // @route PUT /api/user/update
 // @access Private
-export const updateUser = (req: Request, res: Response, next: NextFunction) => {
-	// logic for updating a users account
+export const updateUser = async (
+	req: UserAuthRequest,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		if (!req.params.id) {
+			return res.status(400).json({ message: 'No id provided' })
+		}
+		const user = await User.findById(req.params.id)
+		if (!user) {
+			return res.status(400).json({ message: 'User not found' })
+		}
+		if (req.user?.id !== user.id) {
+			return res
+				.status(401)
+				.json({ message: 'User not authorized to update user' })
+		}
+		if (!req.body || Object.keys(req.body).length === 0) {
+			return res.status(400).json({ message: 'Please add update fields' })
+		}
+		const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+		})
+		return res
+			.status(200)
+			.json({ updatedUser, message: 'User updated successfully ' })
+	} catch (error) {
+		console.error('Error updating user:', error)
+		return res.status(500).json({ message: 'Server error' })
+	}
 }
 
 // @desc Get user account info
