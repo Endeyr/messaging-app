fc53c0c868f11083fde0a866e9b1b74d99d3975a
Endeyr <aaron.thompson42594@gmail.com>
2024-06-17 12:59:42 -0400

    rm length message on password for security

--- client/src/schema/LoginSchema.ts
+++ client/src/schema/LoginSchema.ts
@@ -3,5 +3,5 @@ import { LoginFormDataType } from '../types/Login'
 
 export const loginSchema: ZodType<LoginFormDataType> = z.object({
 	email: z.string().email(),
-	password: z.string().min(8, { message: 'Password is too short' }),
+	password: z.string(),
 })
