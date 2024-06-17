2dd8e90900ad6b5497dacb9e20bcc2b62fe404ca
Endeyr <aaron.thompson42594@gmail.com>
2024-06-17 14:28:17 -0400

    add redirect if not logged in for dashboard

--- client/src/routes/dashboard/index.tsx
+++ client/src/routes/dashboard/index.tsx
@@ -1,6 +1,17 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useAppSelector } from '../../app/hooks'
 // @desc All of users friends, messages, ect?
 // @private Must be logged in
 const DashboardPage = () => {
-	return <div>DashboardPage</div>
+	const navigate = useNavigate()
+	const { user } = useAppSelector((state) => state.auth)
+	useEffect(() => {
+		if (!user) {
+			navigate('/authentication/login')
+		}
+	}, [user, navigate])
+
+	return <div>DashboardPage, {user && user.username}</div>
 }
 export default DashboardPage
