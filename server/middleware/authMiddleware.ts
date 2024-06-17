69de2a51096c3afebff741dce03f6ca8ea2c1202
Endeyr <aaron.thompson42594@gmail.com>
2024-06-17 13:17:45 -0400

    fixed logic and added user check in auth middleware

--- server/middleware/authMiddleware.ts
+++ server/middleware/authMiddleware.ts
@@ -8,27 +8,30 @@ export const protect = async (
 	res: Response,
 	next: NextFunction
 ) => {
-	let token
+	let token: string | undefined
 	if (
 		req.headers.authorization &&
 		req.headers.authorization.startsWith('Bearer')
 	) {
-		try {
-			token = req.headers.authorization.split(' ')[1]
-			const decoded = jwt.verify(
-				token,
-				process.env.JWT_SECRET as string
-			) as jwt.JwtPayload
-			const userReq = req as UserAuthRequest
-			userReq.user = (await User.findById(decoded.userId).select(
-				'-password'
-			)) as IUser
-			next()
-		} catch (error) {
-			res.status(401).json({ message: 'Not authorized' })
-		}
+		token = req.headers.authorization.split(' ')[1]
 	}
 	if (!token) {
-		res.status(401).json({ message: 'Not authorized, missing token' })
+		return res.status(401).json({ message: 'Not authorized, missing token' })
+	}
+	try {
+		const decoded = jwt.verify(
+			token,
+			process.env.JWT_SECRET as string
+		) as jwt.JwtPayload
+		const userReq = req as UserAuthRequest
+		userReq.user = (await User.findById(decoded.userId).select(
+			'-password'
+		)) as IUser
+		if (!userReq.user) {
+			return res.status(401).json({ message: 'Not authorized, user not found' })
+		}
+		next()
+	} catch (error) {
+		res.status(401).json({ message: 'Not authorized' })
 	}
 }
