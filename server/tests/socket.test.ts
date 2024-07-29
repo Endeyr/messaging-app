dab7ca3658de5f234988b00f04464f9f8ae12fbd
Endeyr <aaron.thompson42594@gmail.com>
2024-07-29 09:51:13 -0400

    update base socket test

--- server/tests/socket.test.ts
+++ server/tests/socket.test.ts
@@ -1,6 +1,6 @@
 import { createServer } from 'node:http'
 import { type AddressInfo } from 'node:net'
-import { Server, type Socket as ServerSocket } from 'socket.io'
+import { Server } from 'socket.io'
 import { io as ioc, type Socket as ClientSocket } from 'socket.io-client'
 import app from '..'
 
@@ -64,6 +64,19 @@ describe('Socket Tests', () => {
 			io.on('connection', (socket) => {
 				expect(socket).toBeDefined()
 			})
+		})
+
+		test('should communicate with waiting for socket.io handshakes', (done) => {
+			// Emits from Client to Server
+			clientSocket.emit('user', 'connected')
+			// Use timeout to wait for socket.io server handshakes
+			setTimeout(() => {
+				// Put your server side expect() here
+				io.on('user', (msg) => {
+					expect(msg).toBe('connected')
+				})
+				done()
+			}, 50)
 		})
 	})
 })
