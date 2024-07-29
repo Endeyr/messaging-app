e144d0aa5aff992ca05d121d8886bf62eb04ea28
Endeyr <aaron.thompson42594@gmail.com>
2024-07-29 15:32:14 -0400

    update api test for coverage

--- server/tests/api.test.ts
+++ server/tests/api.test.ts
@@ -12,87 +12,132 @@ import {
 
 describe('API Tests', () => {
 	describe('User Routes /api/user', () => {
-		it('should register a new user', async () => {
-			const response = await supertest(app)
-				.post('/api/user/register')
-				.send({
-					username: 'test1user',
-					email: 'test1@example.com',
-					password: 'password1233',
-					confirmPassword: 'password1233',
-					role: [RoleEnum.user],
-				})
-			expect(response.status).toBe(201)
-			expect(response.body.data).toHaveProperty('userId')
-			expect(response.body.data.email).toBe('test1@example.com')
-		})
-		it('should login a current user', async () => {
-			const response = await supertest(app).post('/api/user/login').send({
-				email: 'mockUser@example.com',
-				password: 'password1233',
+		describe('registerUser', () => {
+			it('should return if user already registered', () => {})
+			it('should catch error saving new user', () => {})
+			it('should catch error registering user ', () => {})
+			it('should register a new user', async () => {
+				const response = await supertest(app)
+					.post('/api/user/register')
+					.send({
+						username: 'test1user',
+						email: 'test1@example.com',
+						password: 'password1233',
+						confirmPassword: 'password1233',
+						role: [RoleEnum.user],
+					})
+				expect(response.status).toBe(201)
+				expect(response.body.data).toHaveProperty('userId')
+				expect(response.body.data.email).toBe('test1@example.com')
 			})
-			expect(response.status).toBe(200)
-			expect(response.body.data).toHaveProperty('token')
-			expect(response.body.data.email).toBe('mockUser@example.com')
 		})
-		it('should update a user', async () => {
-			const id = mockUser2._id
-			const response = await supertest(app)
-				.put(`/api/user/update/${id}`)
-				.set('Authorization', `Bearer ${user2Token}`)
-				.send({
-					email: 'test2@example.com',
+		describe('loginUser', () => {
+			it('should catch error finding user from db', () => {})
+			it('should catch error finding user by email', () => {})
+			it('should catch error if password is incorrect', () => {})
+			it('should catch error generating token', () => {})
+			it('should login a current user', async () => {
+				const response = await supertest(app).post('/api/user/login').send({
+					email: 'mockUser@example.com',
+					password: 'password1233',
 				})
-			expect(response.status).toBe(200)
-			expect(response.body.updatedUser.email).toBe('test2@example.com')
-			expect(response.body.message).toBe('User updated successfully')
+				expect(response.status).toBe(200)
+				expect(response.body.data).toHaveProperty('token')
+				expect(response.body.data.email).toBe('mockUser@example.com')
+			})
 		})
-		it('should get a user data', async () => {
-			const id = mockAdmin._id
-			const response = await supertest(app)
-				.get(`/api/user/accessUser/${id}`)
-				.set('Authorization', `Bearer ${authToken}`)
-			expect(response.status).toBe(200)
-			expect(response.body).toHaveProperty('userId')
-			expect(response.body.email).toBe('mockAdmin@example.com')
+		describe('updateUser', () => {
+			it('should catch error updating user', () => {})
+			it('should return if id not in params', () => {})
+			it('should return if user not found in db', () => {})
+			it('should return if user not found in request', () => {})
+			it('should return if user not authorized', () => {})
+			it('should return if request missing update fields', () => {})
+			it('should update a user', async () => {
+				const id = mockUser2._id
+				const response = await supertest(app)
+					.put(`/api/user/update/${id}`)
+					.set('Authorization', `Bearer ${user2Token}`)
+					.send({
+						email: 'test2@example.com',
+					})
+				expect(response.status).toBe(200)
+				expect(response.body.updatedUser.email).toBe('test2@example.com')
+				expect(response.body.message).toBe('User updated successfully')
+			})
 		})
-		it('should delete a user', async () => {
-			const id = mockUser2._id
-			const response = await supertest(app)
-				.delete(`/api/user/delete/${id}`)
-				.set('Authorization', `Bearer ${authToken}`)
-			expect(response.status).toBe(200)
-			expect(response.body.message).toBe('User deleted successfully')
+		describe('accessUserData', () => {
+			it('should return if error accessing user data', () => {})
+			it('should return if user not in request', () => {})
+			it('should get a user data', async () => {
+				const id = mockAdmin._id
+				const response = await supertest(app)
+					.get(`/api/user/accessUser/${id}`)
+					.set('Authorization', `Bearer ${authToken}`)
+				expect(response.status).toBe(200)
+				expect(response.body).toHaveProperty('userId')
+				expect(response.body.email).toBe('mockAdmin@example.com')
+			})
+		})
+		describe('deleteUser', () => {
+			it('should catch error deleting user', () => {})
+			it('should return if no id in params', () => {})
+			it('should return if user not authorized', () => {})
+			it('should delete a user', async () => {
+				const id = mockUser2._id
+				const response = await supertest(app)
+					.delete(`/api/user/delete/${id}`)
+					.set('Authorization', `Bearer ${authToken}`)
+				expect(response.status).toBe(200)
+				expect(response.body.message).toBe('User deleted successfully')
+			})
 		})
 	})
 
 	describe('Message Routes', () => {
-		it('should get messages', async () => {
-			const response = await supertest(app)
-				.get('/message/')
-				.set('Authorization', `Bearer ${userToken}`)
-			expect(response.status).toBe(200)
-			expect(response.body.messages).toHaveLength(2)
+		describe('getMessage', () => {
+			it('should return user not found', () => {})
+			it('should return 0 on sort', () => {})
+			it('should catch error fetching messages', () => {})
+			it('should get messages', async () => {
+				const response = await supertest(app)
+					.get('/message/')
+					.set('Authorization', `Bearer ${userToken}`)
+				expect(response.status).toBe(200)
+				expect(response.body.messages).toHaveLength(2)
+			})
 		})
-		it('should post a message', async () => {
-			const response = await supertest(app)
-				.post('/message/')
-				.set('Authorization', `Bearer ${userToken}`)
-				.send({
-					sent_to: mockAdmin,
-					text: 'Per my last message',
-				})
-			expect(response.status).toBe(200)
-			expect(response.body.message).toHaveProperty('sent_from')
-			expect(response.body.message.text).toBe('Per my last message')
+		describe('sendMessage', () => {
+			it('should return if missing body text', () => {})
+			it('should return if user not found', () => {})
+			it('should catch error sending message', () => {})
+			it('should post a message', async () => {
+				const response = await supertest(app)
+					.post('/message/')
+					.set('Authorization', `Bearer ${userToken}`)
+					.send({
+						sent_to: mockAdmin,
+						text: 'Per my last message',
+					})
+				expect(response.status).toBe(200)
+				expect(response.body.message).toHaveProperty('sent_from')
+				expect(response.body.message.text).toBe('Per my last message')
+			})
 		})
-		it('should delete a message', async () => {
-			const id = mockMessage1._id
-			const response = await supertest(app)
-				.delete(`/message/${id}`)
-				.set('Authorization', `Bearer ${userToken}`)
-			expect(response.status).toBe(200)
-			expect(response.body.message).toBe('Message deleted successfully')
+		describe('deleteMessage', () => {
+			it('should return if missing id in params', () => {})
+			it('should return if message not found', () => {})
+			it('should return if user not logged in', () => {})
+			it('should return if user not found', () => {})
+			it('should catch error deleting message', () => {})
+			it('should delete a message', async () => {
+				const id = mockMessage1._id
+				const response = await supertest(app)
+					.delete(`/message/${id}`)
+					.set('Authorization', `Bearer ${userToken}`)
+				expect(response.status).toBe(200)
+				expect(response.body.message).toBe('Message deleted successfully')
+			})
 		})
 	})
 })
