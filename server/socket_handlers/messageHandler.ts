e44271b830ccbd4890859cda7691c1f221ea2b0e
Endeyr <aaron.thompson42594@gmail.com>
2024-08-11 10:25:05 -0400

    updated for socket.io on message

--- server/socket_handlers/messageHandler.ts
+++ server/socket_handlers/messageHandler.ts
@@ -4,7 +4,7 @@ import { type IMessageDocument } from './../model/messages'
 export const messageHandler = (io: Server, socket: Socket): void => {
 	const messageSent = (message: IMessageDocument) => {
 		console.log(message)
-		io.emit('message-received', message) // broadcast to others in room or private
+		io.emit('message-received', `${socket.id} said ${message}`) // broadcast to others in room or private
 	}
 	const messageReceived = (message: IMessageDocument) => {
 		io.emit('message-sent', message)
