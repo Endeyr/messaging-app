49ea8c1a961fcc221f858776a04cb9cad3901368
Endeyr <aaron.thompson42594@gmail.com>
2024-08-22 06:51:12 -0400

    rm todo list

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,6 @@
 import Home from './components/Home'
 
 function App() {
-	// TODO Add user authentication
-	// TODO Add user real time messaging
-	// TODO Add user custom profiles
-	// TODO Deploy to Vercel
-	// TODO Add users can send images
-	// TODO Add users can add friends + see who is online
-	// TODO Add users to group chat
-	// TODO Customize theme + style app
 	return (
 		<>
 			<Home />
